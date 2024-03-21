20c2047fa03b5d9ebbda11af99b3c34fd10be5ea
Budkov Denis <Chukcha2017@bk.ru>
2024-03-22 00:17:09 +0400

    side button2

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let side = document.querySelector('.side');
 let burgerButton = document.querySelector('.header__burger');
+let header = document.querySelector('.header');
 
 
 
 burgerButton.addEventListener('click', function ()  {
-   side.classList.toggle('side_open')
+   side.classList.toggle('side_open');
+
+   header.classList.toggle('side_open')
+   
 });
