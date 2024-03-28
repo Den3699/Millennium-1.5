cc776a9c92738a1874c4e0399eeb700de65ea886
Budkov Denis <Chukcha2017@bk.ru>
2024-03-28 21:14:36 +0400

    Side final

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let side = document.querySelector('.side');
 let burgerButton = document.querySelector('.header__burger');
 let header = document.querySelector('.header');
+let burgerClose = document.querySelector('.side__close');
+// let home = document.querySelector('.home__container.side_open');
 
 
 
 burgerButton.addEventListener('click', function ()  {
    side.classList.toggle('side_open');
 
-   header.classList.toggle('side_open')
+   header.classList.toggle('side_open');
+
    
+   
+});
+
+
+burgerClose.addEventListener('click', function ()  {
+   document.querySelector('.side').classList.remove('side_open')
+});
+
+
+burgerButton.addEventListener('click', event => {
+   event._isClickWithInMenu = true;
+});
+
+side.addEventListener('click', event => {
+   event._isClickWithInMenu = true;
+});
+
+document.body.addEventListener('click', event => {
+   if (event._isClickWithInMenu) return;
+   document.querySelector('.side').classList.remove('side_open')
+
 });
